b8d6aa5d1374b9215a5621391fb5ec2dadcab970
John James Libron <johnjames.libron@gmail.com>
2022-03-16 10:58:34 +0800

    Optimizing list component with useMemo()

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 
+import DemoList from "./components/Demo/DemoList";
 import DemoOutput from "./components/Demo/DemoOutput";
 import Button from "./components/UI/Button/Button";
 import "./App.css";
@@ -7,6 +8,7 @@ import "./App.css";
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
   const [allowToggle, setAllowToggle] = useState(false);
+  const [listTitle, setListTitle] = useState("My List");
 
   console.log("APP RUNNING");
 
@@ -20,12 +22,23 @@ function App() {
     setAllowToggle(true);
   };
 
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New Title");
+  }, []);
+
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <DemoOutput show={showParagraph} />
       <Button onClick={allowToggleHandler}>Allow Toggle!</Button>
+      <br />
+      <br />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+      <br />
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
 }
